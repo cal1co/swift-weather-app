0d6875897d8df29ce62d6efdada563e1c58fcca0
Alex King <calicoalix@gmail.com>
2022-08-20 19:23:41 +1000

    Initial Commit

---
+++ weather-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weather-app
+//
+//  Created by Alex King on R 4/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Alex King on R 4/08/20.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
